a9406946bb0f87dd36a35bd5f3253741a410059d
Bruno Luis de Oliveira <brunoliveira.ca@gmail.com>
2025-06-04 22:09:00 -0300

    feat: add HowItWorks component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import Products from './components/Products'
 import About from './components/About'
+import HowItWorks from './components/HowItWorks'
+import Testimonial from './components/Testimonial'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
       <Features />
       <Products />
       <About />
+      <HowItWorks />
     </>
   )
 }
